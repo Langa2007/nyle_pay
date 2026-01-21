2200670a6d0a0ba13d57cc17abbce20394f1c454
Langa2007 <fidellanga67@gmail.com>
2026-01-21 17:21:08 +0300

    Fixed JPA query method for wallet balances map

--- src/main/java/com/nyle/nylepay/repositories/WalletRepository.java
+++ src/main/java/com/nyle/nylepay/repositories/WalletRepository.java
@@ -3,6 +3,7 @@
 package com.nyle.nylepay.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nyle.nylepay.models.Wallet;
@@ -12,5 +13,6 @@ import java.util.Optional;
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
     Optional<Wallet> findByUserId(Long userId);
+    @Query("SELECT w FROM Wallet w WHERE w.userId = ?1 AND KEY(w.balances) = ?2")
     Optional<Wallet> findByUserIdAndBalances_CurrencyCode(Long userId, String currencyCode);
 }
